3a8dd3d2ccdfd710a616c1632a241e23de633e7f
khiba-k <khibakoenane@gmail.com>
2025-04-08 11:11:32 +0200

    task: Task3, created route for creating new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,6 @@
+import { createHash } from "crypto";
 import dbClient from "../utils/db.mjs";
+
 const postNew = async (email, password) => {
   try {
     const emailExists = await dbClient.checkEmail(email);
@@ -6,9 +8,13 @@ const postNew = async (email, password) => {
     if (emailExists >= 1) {
       return false;
     } else {
+      const hashedPass = createHash("sha1").update(password).digest("hex");
+      const userId = await dbClient.createUser(email, hashedPass);
+
+      return { id: userId, email: email };
     }
   } catch (error) {
-    console.log("An error occured: ", error);
+    console.log("An error occured while posting new user: ", error);
   }
 };
 
